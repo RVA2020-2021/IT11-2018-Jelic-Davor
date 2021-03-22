3ad73e16038c9f958a27d7bb92dcb90d4a00b52a
Davor <Davor@Davor4540s>
2021-03-22 16:39:16 +0100

    Klasa proizvodjac i metode za nju

--- Starter-project/src/main/java/rva/ctrls/RacunRestController.java
+++ Starter-project/src/main/java/rva/ctrls/RacunRestController.java
@@ -3,8 +3,15 @@ package rva.ctrls;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rva.jpa.Racun;
@@ -15,6 +22,9 @@ public class RacunRestController {
 	
 	@Autowired //kreira bin i tako omogucava dependencyInjection
 	private RacunRepository racunRepository;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate; 
 	
 	@GetMapping("racun")
 	public Collection<Racun> getRacuni(){ //vraca sve racune
@@ -29,5 +39,36 @@ public class RacunRestController {
 	@GetMapping("racunNacinPlacanja/{nacinPlacanja}")
 	public Collection<Racun> getRacunByNacinPlacanja(@PathVariable() String nacinPlacanja) { //vraca racune po nacinu placanja
 		return racunRepository.findByNacinPlacanjaContainingIgnoreCase(nacinPlacanja);
+	}
+	
+	@PostMapping("racun")
+	public ResponseEntity<Racun> insertRacun(@RequestBody Racun racun){
+		if(!racunRepository.existsById(racun.getId())) {
+			racunRepository.save(racun);
+			return new ResponseEntity<Racun>(HttpStatus.OK);
+		}
+		return new ResponseEntity<Racun>(HttpStatus.CONFLICT);
+	}
+	
+	@PutMapping("racun")
+	public ResponseEntity<Racun> updateRacun(@RequestBody Racun racun){
+		if(!racunRepository.existsById(racun.getId())) {
+			return new ResponseEntity<Racun>(HttpStatus.NO_CONTENT);
+		}
+		racunRepository.save(racun);
+		return new ResponseEntity<Racun>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("racun/{id}")
+	public ResponseEntity<Racun> deleteRacun(@PathVariable("id") Integer id){
+		if(!racunRepository.existsById(id)) {
+			return new ResponseEntity<Racun>(HttpStatus.NO_CONTENT);
+		}
+		racunRepository.deleteById(id);
+		if(id == -100) {
+			jdbcTemplate.execute("INSERT INTO \"racun\" (\"id\", \"datum\", \"nacin_placanja\") " 
+		+ "VALUES (-100, to_date('01.01.2020.', 'dd.mm.yyyy.'), 'gotovina')");
+		}
+		return new ResponseEntity<Racun>(HttpStatus.OK);
 	}
 }
