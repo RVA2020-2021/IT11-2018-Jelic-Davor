063b3e81c0aa93f2b60d0c9153f6e6a72c8ffdba
DavorJelic <davorjelic@uns.ac.rs>
2021-06-08 12:07:58 +0200

    Test 2

--- Frontend/src/app/app.constants.ts
+++ Frontend/src/app/app.constants.ts
@@ -1,7 +1,7 @@
-export const Proizvodjac_URL = 'https://rva-projekat.herokuapp.com//proizvodjac';
-export const Racun_URL = 'https://rva-projekat.herokuapp.com//racun';
-export const Proizvod_URL = 'https://rva-projekat.herokuapp.com//proizvod';
+export const Proizvodjac_URL = 'https://rva-projekat.herokuapp.com/proizvodjac';
+export const Racun_URL = 'https://rva-projekat.herokuapp.com/racun';
+export const Proizvod_URL = 'https://rva-projekat.herokuapp.com/proizvod';
 
-export const StavkaRacuna_URL = 'https://rva-projekat.herokuapp.com//stavkaRacuna';
-export const StavkeZaRacun_URL = 'https://rva-projekat.herokuapp.com//stavkaZaRacunID';
+export const StavkaRacuna_URL = 'https://rva-projekat.herokuapp.com/stavkaRacuna';
+export const StavkeZaRacun_URL = 'https://rva-projekat.herokuapp.com/stavkaZaRacunID';
 
